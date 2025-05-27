affe3a82e29f85a5fe767a8d6251f03d6c16c0fb
oscarsloan20 <96237875+oscarsloan20@users.noreply.github.com>
2025-05-27 13:12:56 +0100

    On branch main 	new file:   romax/.gitignore 	new file:   romax/README.md 	new file:   romax/package-lock.json 	new file:   romax/package.json 	new file:   romax/public/favicon.ico 	new file:   romax/public/index.html 	new file:   romax/public/logo192.png 	new file:   romax/public/logo512.png 	new file:   romax/public/manifest.json 	new file:   romax/public/robots.txt 	new file:   romax/src/App.css 	new file:   romax/src/App.jsx 	new file:   romax/src/App.test.js 	new file:   romax/src/forms/merc.jsx 	new file:   romax/src/forms/template.jsx 	new file:   romax/src/index.css 	new file:   romax/src/index.js 	new file:   romax/src/logo.svg 	new file:   romax/src/reportWebVitals.js 	new file:   romax/src/setupTests.js

---
+++ romax/src/forms/template.jsx
@@ -0,0 +1,15 @@
+<form>
+    <label>
+        Name:
+        <input type="text" name="name" />
+    </label>
+    <label>
+        Email:
+        <input type="text" name="email" />
+    </label>
+    <label>
+        Phone:
+        <input type ="text" name="phone" />
+    </label>
+    <button type="submit">Submit</button>
+</form>
