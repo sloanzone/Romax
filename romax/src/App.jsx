0f1e9b59183c0f57638d086e4f5a0c834113ed52
oscarsloan20 <96237875+oscarsloan20@users.noreply.github.com>
2025-05-27 17:07:23 +0100

    On branch main 	modified:   romax/package-lock.json 	modified:   romax/package.json 	modified:   romax/src/App.jsx

--- romax/src/App.jsx
+++ romax/src/App.jsx
@@ -1,13 +1,44 @@
 import './App.css';
-import Template from './forms/template.js'
+import { useState } from "react";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import Template from "./forms/template.js"
 
 function App() {
-  return (
-    <div>
-    <p>Sign up form</p>
-    <Template />
-    </div>
-  );
+
+    const [authData, setAuthData] = useState(null);
+
+     const gContainer = {
+       display: 'flex',
+       flexDirection: 'column',
+       alignItems: 'center',
+       marginTop: '50px'
+     };
+
+     return (
+        <div>
+        <Template />
+
+        <GoogleOAuthProvider clientId={"378012159791-1cphgmo5gab66nd2l1hcrnsk70fuejfq.apps.googleusercontent.com"}>
+         <div style={gContainer}>
+           <GoogleLogin
+             onSuccess={(credentialResponse) => {
+               setAuthData(credentialResponse);
+               console.log("Success!", credentialResponse);
+             }}
+             onError={() => {
+               console.log("Login Failed");
+             }}
+           />
+           {authData && (
+             <div>
+               <p>Credential: {authData.credential}</p>
+               <p>Select By: {authData.select_by}</p>
+             </div>
+           )}
+         </div>
+        </GoogleOAuthProvider>
+        </div>
+     );
 }
 
 export default App;
