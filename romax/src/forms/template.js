e9b2540ba9fd69da5606adcfafa8ff9305edd016
Aayush Ale <gameraayush130@gmail.com>
2025-05-27 18:05:07 +0100

    locally

--- romax/src/forms/template.js
+++ romax/src/forms/template.js
@@ -20,9 +20,9 @@ function Template() {
         console.log('Form Data', formData);
         
         try {
-            const response = await fetch('http://localhost:5000', {
+            const response = await fetch('http://localhost:5050/register', {
                 method: 'POST',
-                body: JSON.stringify(formData)
+                body: JSON.stringify({formData})
                 });
 
         if (!response.ok) {
